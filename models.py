8700c68fadf93d4647ec433c3d51dfad18844102
StanislavRostislav <109461013+StanislavRostislav@users.noreply.github.com>
2026-03-30 08:25:12 +0500

    Add User model and update relationships in models.py

--- models.py
+++ models.py
@@ -2,24 +2,37 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+
+    notes = relationship("Note", back_populates="owner")
+
+
 class Note(Base):
     __tablename__ = "notes"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String)
     subject = Column(String)
     category = Column(String)
     filename = Column(String)
     likes = Column(Integer, default=0)
 
-    comments = relationship("Comment", back_populates="note", cascade="all, delete")
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="notes")
+
+    comments = relationship("Comment", back_populates="note")
 
 
 class Comment(Base):
     __tablename__ = "comments"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     text = Column(String)
-    note_id = Column(Integer, ForeignKey("notes.id"))
 
+    note_id = Column(Integer, ForeignKey("notes.id"))
     note = relationship("Note", back_populates="comments")
