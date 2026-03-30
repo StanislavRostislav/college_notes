a1f3ce02cb75722c0540a122bd34c3e60703a707
StanislavRostislav <109461013+StanislavRostislav@users.noreply.github.com>
2026-03-30 08:24:51 +0500

    Change database URL to use test.db

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./notes.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
